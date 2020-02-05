d1bcdc1055f620f2e1f571ec6354aabc81fd3b0c
suryanarayanswami <suryanarayan@beehyv.com>
2020-02-05 19:02:23 +0530

    UnUsed service removed

--- api/src/test/java/org/motechproject/wa/api/ut/WashAcademyControllerUnitTest.java
+++ api/src/test/java/org/motechproject/wa/api/ut/WashAcademyControllerUnitTest.java
@@ -7,7 +7,6 @@ import org.motechproject.event.listener.EventRelay;
 import org.motechproject.wa.api.web.BaseController;
 import org.motechproject.wa.api.web.WashAcademyController;
 import org.motechproject.wa.api.web.contract.washAcademy.SaveBookmarkRequest;
-import org.motechproject.wa.washacademy.repository.WaCourseDataService;
 import org.motechproject.wa.washacademy.service.WashAcademyService;
 
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -26,8 +25,6 @@ public class WashAcademyControllerUnitTest {
 
     @Mock
     private EventRelay eventRelay;
-
-    private WaCourseDataService waCourseDataService;
 
     @Before
     public void setup() {
